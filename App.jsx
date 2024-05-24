561ffbc12c0de0f24cbba5e10e0d568376b39191
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-05-24 11:54:32 +0530

    template-app

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['new NativeEventEmitter']);
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home';
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
